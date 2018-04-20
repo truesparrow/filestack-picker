7b1a03877a12fd89ee8dfa8ca6e6945b225f053e
Horia Coman <horia141@gmail.com>
2018-04-20 09:02:03 +0300

    Upgrade to the filepicker-js

---
+++ src/filestack-js.d.ts
@@ -0,0 +1,2 @@
+declare module "filestack-js" {
+}
--- src/filestack-picker.ts
+++ src/filestack-picker.ts
@@ -1,4 +1,4 @@
-import * as filepicker from 'filepicker-js'
+import * as filestack from 'filestack-js'
 
 import { Image, Picture } from '@truesparrow/content-sdk-js'
 
@@ -25,57 +25,78 @@ declare global {
 window.TRUESPARROW_SELECT_IMAGE = window.TRUESPARROW_SELECT_IMAGE || ((key: string, position: number) => {
     return new Promise(
         (resolve, reject) => {
-            (filepicker as any).setKey(key);
-            (filepicker as any).pick({
-                mimetype: 'image/*',
-                services: ['CONVERT', 'COMPUTER', 'FACEBOOK', 'DROPBOX', 'FLICKR'],
-                conversions: ['crop', 'rotate', 'filter'],
-                imageDim: [Picture.MAIN_WIDTH, Picture.MAIN_HEIGHT],
-                cropRatio: 16 / 9,
-                cropForce: true,
-                maxSize: '10485760'
-            }, (blob: any) => {
-                (filepicker as any).convert(blob, {
-                    width: Picture.MAIN_WIDTH,
-                    height: Picture.MAIN_HEIGHT,
-                    fit: 'scale',
-                    format: Picture.FORMAT,
-                    compress: true,
-                    quality: 90,
-                }, (mainBlob: any) => {
-                    (filepicker as any).convert(blob, {
-                        width: Picture.THUMBNAIL_WIDTH,
-                        height: Picture.THUMBNAIL_HEIGHT,
-                        fit: 'scale',
-                        format: Picture.FORMAT,
-                        compress: true,
-                        quality: 90
-                    }, (thumbnailBlob: any) => {
-                        const mainImage = new Image();
-                        mainImage.uri = mainBlob.url;
-                        mainImage.format = Picture.FORMAT;
-                        mainImage.width = Picture.MAIN_WIDTH;
-                        mainImage.height = Picture.MAIN_HEIGHT;
-                        const thumbnailImage = new Image();
-                        thumbnailImage.uri = thumbnailBlob.url;
-                        thumbnailImage.format = Picture.FORMAT;
-                        thumbnailImage.width = Picture.THUMBNAIL_WIDTH;
-                        thumbnailImage.height = Picture.THUMBNAIL_HEIGHT;
-                        const picture = new Picture();
-                        picture.position = position;
-                        picture.mainImage = mainImage;
-                        picture.thumbnailImage = thumbnailImage;
+            const client = (filestack as any).init(key);
+            client
+                .pick({
+                    fromSources: ["local_file_system", "facebook", "instagram", "googledrive", "dropbox", "flickr", "box", "picasa"],
+                    accept: ["image/*"],
+                    lang: 'en',
+                    transformations: {
+                        crop: {
+                            force: true,
+                            aspectRatio: 1.778
+                        },
+                        rotate: true
+                    },
+                    maxSize: 10485760
+                })
+                .then((res: any) => {
+                    const uploaded = res.filesUploaded[0];
 
-                        resolve(picture);
-                    }, (error: Error) => {
-                        reject(error);
+                    const mainSizeUri = client.transform(uploaded.url, {
+                        resize: {
+                            width: Picture.MAIN_WIDTH,
+                            height: Picture.MAIN_HEIGHT,
+                            fit: 'scale',
+                        },
+                        output: {
+                            format: Picture.FORMAT,
+                            compress: true,
+                            quality: 90
+                        }
                     });
-                }, (error: Error) => {
-                    reject(error);
-                });
-            }, (error: Error) => {
-                reject(error);
-            });
+
+                    client
+                        .storeURL(mainSizeUri)
+                        .then((mainRes: any) => {
+                            const thumbnailSizeUri = client.transform(uploaded.url, {
+                                resize: {
+                                    width: Picture.THUMBNAIL_WIDTH,
+                                    height: Picture.THUMBNAIL_HEIGHT,
+                                    fit: 'scale',
+                                },
+                                output: {
+                                    format: Picture.FORMAT,
+                                    compress: true,
+                                    quality: 90
+                                }
+                            });
+
+                            client
+                                .storeURL(thumbnailSizeUri)
+                                .then((thumbnailRes: any) => {
+                                    const mainImage = new Image();
+                                    mainImage.uri = mainRes.url;
+                                    mainImage.format = Picture.FORMAT;
+                                    mainImage.width = Picture.MAIN_WIDTH;
+                                    mainImage.height = Picture.MAIN_HEIGHT;
+                                    const thumbnailImage = new Image();
+                                    thumbnailImage.uri = thumbnailRes.url;
+                                    thumbnailImage.format = Picture.FORMAT;
+                                    thumbnailImage.width = Picture.THUMBNAIL_WIDTH;
+                                    thumbnailImage.height = Picture.THUMBNAIL_HEIGHT;
+                                    const picture = new Picture();
+                                    picture.position = position;
+                                    picture.mainImage = mainImage;
+                                    picture.thumbnailImage = thumbnailImage;
+
+                                    resolve(picture);
+                                })
+                                .catch((err: any) => reject(err));
+                        })
+                        .catch((err: any) => reject(err));
+                })
+                .catch((err: any) => reject(err));
         });
 });
 
